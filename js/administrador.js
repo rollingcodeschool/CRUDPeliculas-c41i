d93d8b1a9bc6b2e343e316358749277e53bfb96d
Emilse Arias <emilsearias@gmail.com>
2023-04-19 18:19:35 -0300

    agregamos una expresion regular que valida la duracion de la pelicula

--- js/administrador.js
+++ js/administrador.js
@@ -24,21 +24,12 @@ function prepararFormulario(e) {
 
 function crearPelicula() {
   //validar los datos del formulario
-  let resumen = sumarioValidacion("algun texto");
+  let resumen = sumarioValidacion(titulo.value, descripcion.value, duracion.value);
   if (resumen.length === 0) {
     // los datos son validos
     //cree el objeto
     const peliculaNueva = new Pelicula(
-      "El Padrino",
-      "Una película sobre una familia mafiosa italiana en Nueva York.",
-      "el_padrino.jpg",
-      "Drama",
-      1972,
-      175,
-      "Estados Unidos",
-      "Francis Ford Coppola",
-      ["Marlon Brando", "Al Pacino", "James Caan"]
-    );
+     titulo.value, descripcion.value, imagen.value, genero.value, anio.value, duracion.value, pais.value, director.value, reparto.value);
     console.log(peliculaNueva); // "El Padrino"
     //la voy agregar en un array
     listaPeliculas.push(peliculaNueva);
--- js/helpers.js
+++ js/helpers.js
@@ -8,10 +8,31 @@ function validarCantidadCaracteres(texto, min, max){
     }
 }
 
-export function sumarioValidacion(titulo){
+
+//https://gjdhgjdhkgjhdkjghdkfgh/jhgdjhgdfjh-gdgfjhhj.jpg
+
+function validarDuracion(tiempo){
+    let patron = /^\d{2,3}$/;
+    if(patron.test(tiempo)){
+        console.log('la expresion regular funciona');
+        return true
+    }else{
+        console.log('la expresion regular fallo');
+        return false
+    }
+}
+
+
+export function sumarioValidacion(titulo, descripcion, duracion){
     let resumen= '';
     if(!validarCantidadCaracteres(titulo,2,100)){
-        resumen += 'El titulo debe tener entre 2 y 100 caracteres'
+        resumen += 'El titulo debe tener entre 2 y 100 caracteres <br>'
+    }
+    if(!validarCantidadCaracteres(descripcion,5,500)){
+        resumen += 'La descripcion debe tener entre 5 y 500 caracteres <br>'
+    }
+    if(!validarDuracion(duracion)){
+        resumen += 'La duracion de la pelicula debe ser minutos (entre 2 y 3 caracteres numericos) <br>'
     }
     return resumen;
 }
