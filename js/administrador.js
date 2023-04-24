430083941a4c416b2d6bdb772fc6fe50738f23f2
Emilse Arias <emilsearias@gmail.com>
2023-04-24 17:14:53 -0300

    agregamos la validacion del anio

--- js/administrador.js
+++ js/administrador.js
@@ -3,16 +3,16 @@ import { sumarioValidacion } from "./helpers.js";
 
 let formularioAdminPelicula = document.getElementById("formPelicula");
 let listaPeliculas = [];
-let codigo = document.getElementById('codigo'),
-titulo = document.getElementById('titulo'),
-descripcion = document.getElementById('descripcion'),
-imagen = document.getElementById('imagen'),
-genero = document.getElementById('genero'),
-anio = document.getElementById('anio'),
-reparto = document.getElementById('reparto'),
-duracion = document.getElementById('duracion'),
-pais = document.getElementById('pais'),
-director = document.getElementById('director')
+let codigo = document.getElementById("codigo"),
+  titulo = document.getElementById("titulo"),
+  descripcion = document.getElementById("descripcion"),
+  imagen = document.getElementById("imagen"),
+  genero = document.getElementById("genero"),
+  anio = document.getElementById("anio"),
+  reparto = document.getElementById("reparto"),
+  duracion = document.getElementById("duracion"),
+  pais = document.getElementById("pais"),
+  director = document.getElementById("director");
 
 formularioAdminPelicula.addEventListener("submit", prepararFormulario);
 
@@ -24,12 +24,29 @@ function prepararFormulario(e) {
 
 function crearPelicula() {
   //validar los datos del formulario
-  let resumen = sumarioValidacion(titulo.value, descripcion.value, duracion.value, imagen.value);
+
+  let resumen = sumarioValidacion(
+    titulo.value,
+    descripcion.value,
+    duracion.value,
+    imagen.value,
+    anio.value
+  );
+
   if (resumen.length === 0) {
     // los datos son validos
     //cree el objeto
     const peliculaNueva = new Pelicula(
-     titulo.value, descripcion.value, imagen.value, genero.value, anio.value, duracion.value, pais.value, director.value, reparto.value);
+      titulo.value,
+      descripcion.value,
+      imagen.value,
+      genero.value,
+      anio.value,
+      duracion.value,
+      pais.value,
+      director.value,
+      reparto.value
+    );
     console.log(peliculaNueva); // "El Padrino"
     //la voy agregar en un array
     listaPeliculas.push(peliculaNueva);
@@ -39,8 +56,8 @@ function crearPelicula() {
     //cerrar el modal con el formulario
   } else {
     // mostrar al usuario el cartel de error
-    let alerta = document.getElementById('alerta');
+    let alerta = document.getElementById("alerta");
     alerta.innerHTML = resumen;
-    alerta.className = 'alert alert-danger mt-3'
+    alerta.className = "alert alert-danger mt-3";
   }
 }
--- js/helpers.js
+++ js/helpers.js
@@ -1,52 +1,65 @@
-function validarCantidadCaracteres(texto, min, max){
-    if( texto.length >= min && texto.length <= max){
-        console.log('dato correcto');
-        return true;
-    }else{
-        console.log('dato erroneo');
-        return false
-    }
+function validarCantidadCaracteres(texto, min, max) {
+  if (texto.length >= min && texto.length <= max) {
+    console.log("dato correcto");
+    return true;
+  } else {
+    console.log("dato erroneo");
+    return false;
+  }
 }
-
 
 //https://gjdhgjdhkgjhdkjghdkfgh/jhgdjhgdfjh-gdgfjhhj.jpg
 
-function validarDuracion(tiempo){
-    let patron = /^\d{2,3}$/;
-    if(patron.test(tiempo)){
-        console.log('la expresion regular funciona');
-        return true
-    }else{
-        console.log('la expresion regular fallo');
-        return false
-    }
+function validarDuracion(tiempo) {
+  let patron = /^\d{2,3}$/;
+  if (patron.test(tiempo)) {
+    console.log("la expresion regular funciona");
+    return true;
+  } else {
+    console.log("la expresion regular fallo");
+    return false;
+  }
 }
-function validarURLImagen(imagen){
-    let patron = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)$/;
-    if(patron.test(imagen)){
-        console.log('la expresion regular de la imagen funciona');
-        return true
-    }else{
-        console.log('la expresion regular de la imagen fallo');
-        return false
-    }
+function validarURLImagen(imagen) {
+  let patron = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)$/;
+  if (patron.test(imagen)) {
+    console.log("la expresion regular de la imagen funciona");
+    return true;
+  } else {
+    console.log("la expresion regular de la imagen fallo");
+    return false;
+  }
 }
 
+function validarAnio(anio) {
+  const anioMinimo = 1987;
+  const anioMaximo = new Date().getFullYear();
+  if(anio>= anioMinimo && anio <= (anioMaximo + 1)){
+    return true
+  }else{
+    return false;
+  }
+}
 
-
-export function sumarioValidacion(titulo, descripcion, duracion, imagen){
-    let resumen= '';
-    if(!validarCantidadCaracteres(titulo,2,100)){
-        resumen += 'El titulo debe tener entre 2 y 100 caracteres <br>'
-    }
-    if(!validarCantidadCaracteres(descripcion,5,500)){
-        resumen += 'La descripcion debe tener entre 5 y 500 caracteres <br>'
-    }
-    if(!validarDuracion(duracion)){
-        resumen += 'La duracion de la pelicula debe ser minutos (entre 2 y 3 caracteres numericos) <br>'
-    }
-    if(!validarURLImagen(imagen)){
-        resumen += 'La imagen de la pelicula debe ser una URL valida terminada en (.jpg, .png o .gif) <br>'
-    }
-    return resumen;
+export function sumarioValidacion(titulo, descripcion, duracion, imagen, anio) {
+  let resumen = "";
+  if (!validarCantidadCaracteres(titulo, 2, 100)) {
+    resumen += "El titulo debe tener entre 2 y 100 caracteres <br>";
+  }
+  if (!validarCantidadCaracteres(descripcion, 5, 500)) {
+    resumen += "La descripcion debe tener entre 5 y 500 caracteres <br>";
+  }
+  if (!validarDuracion(duracion)) {
+    resumen +=
+      "La duracion de la pelicula debe ser minutos (entre 2 y 3 caracteres numericos) <br>";
+  }
+  if (!validarURLImagen(imagen)) {
+    resumen +=
+      "La imagen de la pelicula debe ser una URL valida terminada en (.jpg, .png o .gif) <br>";
+  }
+  if (!validarAnio(anio)) {
+    resumen +=
+      "El año debe ser entre 1987 y "+ (new Date().getFullYear() + 1 )+" <br>";
+  }
+  return resumen;
 }
